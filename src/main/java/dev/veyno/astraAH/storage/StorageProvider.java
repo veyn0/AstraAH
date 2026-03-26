63735864c853561c6ffb587ea1fe4a3c6117504d
veyn0 <veynoplugins@gmail.com>
2026-03-26 18:23:39 +0100

    continue development

--- src/main/java/dev/veyno/astraAH/storage/StorageProvider.java
+++ src/main/java/dev/veyno/astraAH/storage/StorageProvider.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface StorageProvider {
-    void saveListing(Listing listing);
+    void saveListing(Listing listing) throws Exception;
     Listing getListing(UUID listingId);
     List<Listing> getListings();
     void removeListing(UUID listingId);
--- src/main/java/dev/veyno/astraAH/storage/provider/FileStorageProvider.java
+++ src/main/java/dev/veyno/astraAH/storage/provider/FileStorageProvider.java
@@ -3,8 +3,12 @@ package dev.veyno.astraAH.storage.provider;
 import dev.veyno.astraAH.AstraAH;
 import dev.veyno.astraAH.ah.Listing;
 import dev.veyno.astraAH.storage.StorageProvider;
+import dev.veyno.astraAH.util.ItemStackUtil;
 import dev.veyno.astraAH.util.YamlStorage;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,23 +24,56 @@ public class FileStorageProvider implements StorageProvider {
     }
 
     @Override
-    public void saveListing(Listing listing){
-
+    public void saveListing(Listing listing) throws Exception {
+        synchronized (storage.getLock()) {
+            FileConfiguration config = storage.getFileConfiguration();
+            String path = "listings."+listing.listingId();
+            config.set(path+".listingId", listing.listingId());
+            config.set(path+".price", listing.price());
+            config.set(path+".playerId", listing.playerId());
+            config.set(path+".content", ItemStackUtil.itemToBase64(listing.content()));
+        }
     }
 
     @Override
     public Listing getListing(UUID listingId) {
-        return null;
+        synchronized (storage.getLock()) {
+            return getListingById(listingId);
+        }
     }
 
     @Override
     public List<Listing> getListings(){
-        return null;
+        synchronized (storage.getLock()) {
+            FileConfiguration config = storage.getFileConfiguration();
+            List<Listing> result = new ArrayList<>();
+            for(String key : config.getConfigurationSection("listings").getKeys(false)){
+            result.add(getListingById(UUID.fromString(key)));
+            }
+            return result.isEmpty() ? null : result;
+        }
     }
 
     @Override
     public void removeListing(UUID listingId){
-
+        synchronized (storage.getLock()){
+            FileConfiguration config = storage.getFileConfiguration();
+            config.set("listings."+listingId, null);
+        }
+        storage.saveFileAsync();
     }
 
+    private Listing getListingById(UUID listingId){
+        try {
+            String path = "listings."+listingId;
+            FileConfiguration config = storage.getFileConfiguration();
+            double price = config.getDouble(path+".price");
+            UUID playerId = UUID.fromString(config.getString(path+".playerId"));
+            ItemStack content = ItemStackUtil.itemFromBase64(config.getString(path+".content"));
+            return new Listing(listingId, playerId, price, content);
+        }
+        catch (Exception e){
+            return null;
+        }
+    }
 }
