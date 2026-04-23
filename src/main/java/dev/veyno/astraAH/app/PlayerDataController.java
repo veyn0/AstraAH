29513a1f3e24bece4bc7c9c472253635e7d334ef
mattes <veynoplugins@gmail.com>
2026-04-23 11:55:03 +0200

    .

--- src/main/java/dev/veyno/astraAH/app/PlayerDataController.java
+++ src/main/java/dev/veyno/astraAH/app/PlayerDataController.java
@@ -10,18 +10,19 @@ public class PlayerDataController {
 
     private AstraAH plugin;
     private PlayerDataService playerDataService;
+    private PlayerDataManager playerDataManager;
 
     public PlayerDataController(AstraAH plugin, PlayerDataService playerDataService) {
         this.plugin = plugin;
         this.playerDataService = playerDataService;
+        this.playerDataManager = new PlayerDataManager(plugin, playerDataService);
     }
 
     public PlayerData getPlayerData(UUID playerId){
         return playerDataService.getPlayerData(playerId);
     }
 
-    public PlayerDataService getPlayerDataService() {
-        return playerDataService;
+    public PlayerDataManager getPlayerDataManager() {
+        return playerDataManager;
     }
-
 }
