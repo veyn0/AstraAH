c567c488e8f7d347b2d46c4e37339309c6ce01af
mattes <veynoplugins@gmail.com>
2026-05-04 00:35:01 +0200

    .

---
+++ src/main/java/dev/veyno/astraAH/ui/pages/CategoryEditPage.java
@@ -0,0 +1,32 @@
+package dev.veyno.astraAH.ui.pages;
+
+import dev.veyno.astraAH.ui.Page;
+import net.kyori.adventure.text.Component;
+
+public class CategoryEditPage implements Page {
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void reload() {
+
+    }
+
+    @Override
+    public void invalidate(Section section) {
+
+    }
+
+    @Override
+    public void buildOnce() {
+
+    }
+
+    @Override
+    public Component getPageTitle() {
+        return null;
+    }
+}
